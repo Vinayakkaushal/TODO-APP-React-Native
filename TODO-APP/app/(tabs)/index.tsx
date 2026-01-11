1b871e0666e0d39c47ddee2fb463298d677ae26b
Vinayak Kaushal <vinayakkaushal935@gmail.com>
2026-01-11 20:12:06 +0530

    Created theme custumization

---
+++ TODO-APP/app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import useTheme from "@/hooks/useTheme";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function Index() {
+  const {toggleDarkMode} = useTheme();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}></Text>
+
+      <TouchableOpacity onPress={toggleDarkMode}><Text>
+        Toggle the mode
+        </Text></TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    fontSize: 40,
+  },
+});
