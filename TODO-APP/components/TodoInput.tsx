7ded1f58083c2f68a316f939a9fb8a6d29388f61
Vinayak Kaushal <vinayakkaushal935@gmail.com>
2026-01-21 23:54:12 +0530

    made changes

--- TODO-APP/components/TodoInput.tsx
+++ TODO-APP/components/TodoInput.tsx
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import useTheme from '@/hooks/useTheme'
+import { createHomeStyles } from '@/assets/styles/home.styles';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const TodoInput = () => {
-    
-  return (
+const {colors} = useTheme();
+
+const homeStyles = createHomeStyles(colors);
+const [newTodo , setNewTodo] = useState("");
+const addTodo = useMutation(api.todos.addTodos);
+
+const handleAddTodo = async ()=>{
+  
+}
+
+return (
     <View>
-      <Text>TodoInput</Text>
+        <Text>TodoInput</Text>
     </View>
   )
 }
