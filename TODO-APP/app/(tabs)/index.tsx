206ea3264ef825eb409dbb45d63156af7cba2b4b
Vinayak Kaushal <vinayakkaushal935@gmail.com>
2026-01-22 23:43:46 +0530

    made changes

--- TODO-APP/app/(tabs)/index.tsx
+++ TODO-APP/app/(tabs)/index.tsx
@@ -5,10 +5,19 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "@/components/Header";
 import TodoInput from "@/components/TodoInput";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
   const homeStyles = createHomeStyles(colors);
+
+  const todos = useQuery(api.todos.getTodos)
+
+  const isLoading = todos===undefined;
+
+  if(isLoading) return <LoadingSpinner/>
 
   return (
     <LinearGradient
@@ -19,9 +28,7 @@ export default function Index() {
       <SafeAreaView style={homeStyles.safeArea}>
         <Header />
         <TodoInput />
-        <TouchableOpacity onPress={toggleDarkMode}>
-          <Text>Toggle the mode</Text>
-        </TouchableOpacity>
+       
       </SafeAreaView>
     </LinearGradient>
   );
